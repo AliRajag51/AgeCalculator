05ebe0dac9ff72e23e8768e2a74f06221ac195f3
Ali Haider <rajaalihaider534@gmail.com>
2024-07-06 00:10:04 -0700

    AgeCalculator is Done in C# Windows Form Application.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.calculateAgebtn = new System.Windows.Forms.Button();
             this.dbo = new System.Windows.Forms.DateTimePicker();
@@ -38,20 +39,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(221, 130);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(159, 150);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(137, 29);
+            this.label1.Size = new System.Drawing.Size(163, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "DateOfBirth";
             this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // calculateAgebtn
             // 
-            this.calculateAgebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.calculateAgebtn.Location = new System.Drawing.Point(507, 181);
+            this.calculateAgebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateAgebtn.Location = new System.Drawing.Point(552, 218);
             this.calculateAgebtn.Name = "calculateAgebtn";
-            this.calculateAgebtn.Size = new System.Drawing.Size(217, 37);
+            this.calculateAgebtn.Size = new System.Drawing.Size(361, 45);
             this.calculateAgebtn.TabIndex = 1;
             this.calculateAgebtn.Text = "CalculateAge";
             this.calculateAgebtn.UseVisualStyleBackColor = true;
@@ -59,29 +60,29 @@
             // 
             // dbo
             // 
-            this.dbo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dbo.Location = new System.Drawing.Point(432, 125);
+            this.dbo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dbo.Location = new System.Drawing.Point(406, 145);
             this.dbo.Name = "dbo";
-            this.dbo.Size = new System.Drawing.Size(292, 35);
+            this.dbo.Size = new System.Drawing.Size(507, 39);
             this.dbo.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(221, 248);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(159, 308);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 29);
+            this.label2.Size = new System.Drawing.Size(132, 32);
             this.label2.TabIndex = 3;
             this.label2.Text = "Your Age";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // currentAgeShow
             // 
-            this.currentAgeShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.currentAgeShow.Location = new System.Drawing.Point(432, 241);
+            this.currentAgeShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentAgeShow.Location = new System.Drawing.Point(406, 301);
             this.currentAgeShow.Name = "currentAgeShow";
-            this.currentAgeShow.Size = new System.Drawing.Size(292, 35);
+            this.currentAgeShow.Size = new System.Drawing.Size(507, 39);
             this.currentAgeShow.TabIndex = 4;
             this.currentAgeShow.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -89,16 +90,18 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::InventoryManagementSystem.Properties.Resources.AgeCalculatorBackground;
+            this.BackgroundImage = global::InventoryManagementSystem.Properties.Resources.AgeCalculatorBackground1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(936, 559);
+            this.ClientSize = new System.Drawing.Size(1054, 536);
             this.Controls.Add(this.currentAgeShow);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dbo);
             this.Controls.Add(this.calculateAgebtn);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "FirstForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Age Calculator";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace InventoryManagementSystem.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap AgeCalculatorBackground1 {
+            get {
+                object obj = ResourceManager.GetObject("AgeCalculatorBackground1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
